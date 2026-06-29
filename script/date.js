8946841f4afc21234acb88d50d615ce892ad180e
ainesider <lcurrovillegas@byupathway.edu>
2026-06-29 13:02:53 +0800

    commit

---
+++ script/date.js
@@ -0,0 +1,7 @@
+
+// CURRENT YEAR
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// LAST MODIFIED
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
