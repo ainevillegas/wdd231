17fbdcc85de14645594ac7a14ab0bbac349a1eef
ainesider <lcurrovillegas@byupathway.edu>
2026-07-24 20:39:03 +0800

    commit

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -4,14 +4,40 @@ if (timestampField) {
     timestampField.value = new Date().toISOString();
 }
 
-const modalButtons = document.querySelectorAll("[data-modal]");
+const openModalButtons = document.querySelectorAll(
+    ".membership-card button[data-modal]"
+);
 
-modalButtons.forEach((button) => {
+openModalButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        const dialog = document.getElementById(button.dataset.modal);
+        const modalId = button.dataset.modal;
+        const dialog = document.getElementById(modalId);
 
         if (dialog) {
             dialog.showModal();
+        }
+    });
+});
+
+const closeModalButtons = document.querySelectorAll(".close-modal");
+
+closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const modalId = button.dataset.modal;
+        const dialog = document.getElementById(modalId);
+
+        if (dialog) {
+            dialog.close();
+        }
+    });
+});
+
+const dialogs = document.querySelectorAll("dialog");
+
+dialogs.forEach((dialog) => {
+    dialog.addEventListener("click", (event) => {
+        if (event.target === dialog) {
+            dialog.close();
         }
     });
 });
